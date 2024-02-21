cee4b43e037a281c1af631608688de4c4ead95f8
Dana <danayarvand8080@gmail.com>
2024-02-21 04:38:00 -0800

    v.1

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        320:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1024:{
+            items:6
+        },
+        1440:{
+            items:7
+        },
+        2560:{
+            items:7
+        },
+        425:{
+            items:4
+        },
+        768:{
+            items:7
+        },
+        375:{
+            items:4
+        },
+    }
+})
